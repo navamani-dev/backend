8598dffc18e939c24a9c0fcaa34a78b2efacb6b6
navamani-dev <142824859+navamani-dev@users.noreply.github.com>
2026-06-30 19:41:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 dotenv.config();
 db();
-const route = require('./routes/routes');
 app.use(express.json());
 app.use(cors());
+
+const route = require('./routes/routes');
 app.use('/api', route);
 
 const port = process.env.PORT || 9090;
